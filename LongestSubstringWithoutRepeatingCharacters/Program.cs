bd167c0feb94709fff644628a99ec120cb1d41d1
wuyuansushen <819070864@qq.com>
2021-03-18 11:11:07 +0800

    Add project files.

---
+++ LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LongestSubstringWithoutRepeatingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abcabcbb";
+            HashSet<char> proc = new HashSet<char>();
+            int n = s.Length;
+
+            int right = -1;
+
+            int output=0;
+
+            for(int i=0;i<n;i++)
+            {
+                if (i != 0)
+                {
+                    proc.Remove(s[i - 1]);
+                }
+                else
+                { }
+                    while(right+1<n&& !proc.Contains(s[right+1]))
+                    {
+                        proc.Add(s[right + 1]);
+                        right++;
+                    }
+                output = right - i + 1 > output ? right - i + 1 : output;
+            }
+                Console.WriteLine(output);
+        }
+    }
+}
